5263623e021b556852a454ddfd0d044a5c4fc7ac
Terry Griffin <terry.griffin@msutexas.edu>
2026-02-08 02:40:51 -0600

    cross your fingers

--- workflow.sh
+++
@@ -1,6 +0,0 @@
-python export_assignments_csv.py
-# edit dates
-python import_assignments_csv.py
-python build_global_calendar.py
-python build_folder_readmes.py
-#git commit -am "Update assignment calendars"
